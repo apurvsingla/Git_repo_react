0443ba607c0a11dce88b4f4a19c4994b0b1875b3
Apurv <apurvsingla@gmail.com>
2020-07-15 14:57:53 +0530

    final

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "fir-1265.firebaseapp.com",
+  databaseURL: "https://fir-1265.firebaseio.com",
+  projectId: "fir-1265",
+  storageBucket: "fir-1265.appspot.com",
+  messagingSenderId: "1025425582860",
+  appId: "1:1025425582860:web:2005ae8e06e8600efa3729",
+  measurementId: "G-0YGKBGC7WL"
+};
+
+export default firebaseConfig;
